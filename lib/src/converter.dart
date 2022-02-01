ad6da941fc916dbef946fad44f7934bf77f960e2
SohailV <sohail2889@live.com>
2022-02-01 17:33:47 -0500

    working in the conveter

---
+++ lib/src/converter.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'package:image/image.dart';
+
+convertImage(FileSystemEntity selectedFile, String format) {
+  final rawImage = (selectedFile as File).readAsBytesSync();
+  final image = decodeImage(rawImage);
+
+  var newImage;
+  if (format == 'jpg') {
+    newImage = encodeJpg(image);
+  } else if (format == 'png') {
+    newImage = encodePng(image);
+  } else {
+    print('Unsupported file type');
+  }
+}
